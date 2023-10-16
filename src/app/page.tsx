895f6d1164fc1ddab20462a5d524ff7da64f45d4
Pedro Moura <peedro.conta2@gmail.com>
2023-10-16 18:40:17 -0300

    Faltando fundo, confeti e certo/errado

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,23 @@
-"use client"
+"use client";
 
 import { PerguntaItem } from "@/components/PerguntaItem";
 import { Results } from "@/components/Results";
 import { questions } from "@/data/question";
-import { Result } from "postcss";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import Lottie, * as lottie from 'lottie-web';
+import confettiAnimation from './../assets/Animation - 1696450388299.json';
+import fundo from './../assets/giphy-downsized-large.gif'
+
+import quiz from "./../assets/quiz.jpg";
 
 const Page: React.FC = () => {
-  
-  const title = 'O quanto você sabe sobre HTML?';
+  const title = "O quanto você sabe sobre HTML?";
 
   const [answers, setAnswers] = useState<number[]>([]);
   const [currentquestions, setCurrentquestions] = useState(0);
   const [showResult, setShowResult] = useState(false);
   const [quizStarted, setQuizStarted] = useState(false);
+  const [confettiVisible, setConfettiVisible] = useState(false);
 
   const loadNextQuestion = () => {
     if (questions[currentquestions + 1]) {
@@ -21,32 +25,55 @@ const Page: React.FC = () => {
     } else {
       setShowResult(true);
     }
-  }
+  };
 
   const handleReiniciar = () => {
     setAnswers([]);
     setCurrentquestions(0);
     setShowResult(false);
     setQuizStarted(false);
-  }
+  };
 
   const handleStartQuiz = () => {
     setQuizStarted(true);
-  }
+  };
 
   const handleAnswered = (answer: number) => {
     setAnswers([...answers, answer]);
     loadNextQuestion();
-  }
+  };
+
+  useEffect(() => {
+    if (showResult) {
+
+      setConfettiVisible(true);
+
+      const confettiTimeout = setTimeout(() => {
+        setConfettiVisible(false);
+      }, 1000); 
+
+      return () => {
+        clearTimeout(confettiTimeout); 
+      };
+    }
+  }, [showResult]);
 
   return (
-    <div className="w-full h-screen flex justify-center items-center bg-blue-400">
-      <div className="w-full max-w-xl rounded-md bg-white text-black shadow shadow-black">
-        <div className="p-5 font-bold text-2xl border border-gray-300 flex justify-center">{title}</div>
+
+    <div className="w-full min-h-screen flex justify-center items-center bg-blue-400">
+      <div className="w-full max-w-xl m-10 rounded-md bg-white text-black shadow shadow-black">
+        <div className="p-5 font-bold text-2xl border border-gray-300 flex justify-center">
+          {title}
+        </div>
         <div className="p-5">
           {!quizStarted && (
             <div className="text-center">
-              <button onClick={handleStartQuiz} className="px-3 py-2 rounded-md bg-blue-500 text-white">Iniciar o Quiz</button>
+              <button
+                onClick={handleStartQuiz}
+                className="px-3 py-2 rounded-md bg-blue-500 text-white hover:opacity-80"
+              >
+                Iniciar o Quiz
+              </button>
             </div>
           )}
           {quizStarted && !showResult && (
@@ -56,21 +83,34 @@ const Page: React.FC = () => {
               onAnswer={handleAnswered}
             />
           )}
-          {showResult && (
-            <Results questions={questions} answers={answers} />
-          )}
+          {showResult && <Results questions={questions} answers={answers} />}
         </div>
         <div className="p-5 text-center border-t border-gray-300">
-          {!showResult && (
-            `${currentquestions + 1} de ${questions.length} pergunta${questions.length === 1 ? '' : 's'}`
-          )}
+          {quizStarted &&
+            !showResult &&
+            `${currentquestions + 1} de ${questions.length} pergunta${
+              questions.length === 1 ? "" : "s"
+            }`}
           {showResult && (
-            <button onClick={handleReiniciar} className="px-3 py-2 rounded-md bg-blue-500 text-white">Reiniciar Quiz</button>
+
+              // <Lottie
+              //   style={{ position: "absolute", zIndex: 999, width: "80%" }}
+              //   animationData={confettiAnimation}
+              //   loop={false}
+              //   onAnimationEnd={() => alert("final")}
+              // />            
+
+            <button
+              onClick={handleReiniciar}
+              className="px-3 py-2 rounded-md bg-blue-500 text-white"
+            >
+              Reiniciar Quiz
+            </button>
           )}
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default Page;
--- src/components/Results.tsx
+++ src/components/Results.tsx
@@ -1,25 +1,31 @@
-import { Question } from "@/types/Question"
+import { Question } from "@/types/Question";
+import correto from "./../assets/correto.png";
+import errado from "./../assets/errado.png";
 
 type Props = {
-    questions: Question[];
-    answers: number[];
-}
+  questions: Question[];
+  answers: number[];
+};
 
 export const Results = ({ questions, answers }: Props) => {
-
-    const acerto = ('0');
-
-    return (
-        <div>
-            {questions.map((item, key) => (
-                <div key={key} className="mb-3">
-                    <div className="font-bold">{key + 1}. {item.question}</div>
-                    <div>
-                        <span>({item.answer === answers[key] ? 'Acertou!' : 'Errou!'}) - </span>
-                        {item.options[item.answer]}
-                    </div>
-                </div>
-            ))}
+  return (
+    <div>
+      {questions.map((item, key) => (
+        <div key={key} className="mb-3">
+          <div className="font-bold">{key + 1}. {item.question}</div>
+            {/* {answers[key] === item.answer ? (
+              <img src={correto} alt="Correto" /> 
+            ) : (
+              <img src={errado} alt="Errado" /> 
+            )} */}
+            {item.options[answers[key]]}
+            {answers[key] === item.answer ? (
+              <div className="text-green-600 font-bold">Resposta correta</div>
+            ) : (
+              <div className="text-red-600 font-bold">Resposta incorreta</div>
+            )}
         </div>
-    )
-}
+      ))}
+    </div>
+  );
+};
